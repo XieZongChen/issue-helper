6d7733ddbae9aec4517ce18f345157662314ab6b
XieZongChen <952937570@qq.com>
2021-05-06 10:45:38 +0800

    feat: 更新第三方库类型声明

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "element3/lib/theme-chalk/index.css";
+import { createApp } from "vue";
+import Element3 from "element3";
+import App from "./App.vue";
+createApp(App).use(Element3).mount("#app");
+//# sourceMappingURL=main.js.map
